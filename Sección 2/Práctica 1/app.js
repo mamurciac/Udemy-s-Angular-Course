b1775bb2151daa37a5a3e7d2008741d75de44dde
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-08-19 22:29:05 -0500

    Práctica Básica

---
+++ Sección 2/Práctica 1/app.js
@@ -0,0 +1,7 @@
+function saludar(nombre) {
+    console.table("Hola " + nombre); //Salida esperada: Hola Logan
+}
+var wolverine = {
+    nombre: "Logan"
+};
+saludar(wolverine.nombre);
---
+++ Sección 2/Práctica 1/app.ts
@@ -0,0 +1,9 @@
+function saludar(nombre : string){
+  console.table("Hola " + nombre); //Salida esperada: Hola Logan
+}
+
+const wolverine = {
+  nombre: "Logan"
+};
+
+saludar(wolverine.nombre);
