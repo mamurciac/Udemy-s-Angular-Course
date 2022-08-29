f67e2787cd04cb006269dc2a0aff6e9f99dd3bdd
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-08-28 21:46:49 -0500

    Práctica Básica

---
+++ Sección 2/Práctica 1/clase4_FuncionesYArgumentos.ts
@@ -0,0 +1,20 @@
+(function(){
+    //Función que tiene un argumento obligatorio, un argumento opcional y un argumento por defecto
+    function activarHabilidad(nombre: string, momento?: string, objeto: string = "batiseñal"){
+        if(momento){
+            console.log(`${nombre} activó el(la) ${objeto} en el(la) ${momento}`);
+        }else{
+            console.log(`${nombre} activó el(la) ${objeto}`);
+        }
+
+        //Forma equivalente de mostrar el mensaje
+        //if(momento != undefined){
+        //    console.log(`${nombre} activó el(la) ${objeto} en el(la) ${momento}`);
+        //}else{
+        //    console.log(`${nombre} activó el(la) ${objeto}`);
+        //}
+    }
+
+    activarHabilidad("Gordon", "noche"); //Se llama la función para ejecutarse
+    activarHabilidad("Gordon"); //Se llama la función para ejecutarse
+})();
