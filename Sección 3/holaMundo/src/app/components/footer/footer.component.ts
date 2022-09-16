8bffceca38e6d25b473334c0644037b1058088dd
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-09-15 20:21:12 -0500

    Edición del Componente Footer (Pie de Página) e Inclusión en el HTML

--- Sección 3/holaMundo/src/app/components/footer/footer.component.ts
+++ Sección 3/holaMundo/src/app/components/footer/footer.component.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  anioActualizacion: number;
+  constructor(){
+    this.anioActualizacion = new Date().getFullYear();
+  }
 
   ngOnInit(): void {
   }
