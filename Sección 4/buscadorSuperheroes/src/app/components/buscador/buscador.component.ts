1a45b1707b9e85d6653e445657ab97abc86b68cf
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-09-23 05:59:10 -0500

    Implementación del Buscador de Superhéroes (Parte Completa)

--- Sección 4/buscadorSuperheroes/src/app/components/buscador/buscador.component.ts
+++ Sección 4/buscadorSuperheroes/src/app/components/buscador/buscador.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HeroesService} from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscador',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buscador.component.css']
 })
 export class BuscadorComponent implements OnInit {
+  heroes: any[] = [];
+  termino: string = "";
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private _heroesService: HeroesService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      console.log(params['termino']);
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.buscarHeroes(params['termino']);
+      console.log(this.heroes);
+    });
   }
 
 }
