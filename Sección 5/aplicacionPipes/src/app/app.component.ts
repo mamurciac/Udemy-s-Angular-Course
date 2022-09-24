215affde047dd283287c3580ac83f2c564a9f9f3
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-09-24 04:19:58 -0500

    Manejo de Pipes en formatos de moneda y representación de objetos JSON

--- Sección 5/aplicacionPipes/src/app/app.component.ts
+++ Sección 5/aplicacionPipes/src/app/app.component.ts
@@ -11,4 +11,15 @@ export class AppComponent {
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   numeroPi: number = Math.PI;
   porcentaje: number = 0.234;
+  salario: number = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Evergreen Terrace',
+      numero: 742
+    }
+  }
 }
