037c494f278394b43cd44e73136d87497266b3a4
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-09-24 05:27:47 -0500

    Implementación de Pipe Personalizado para Capitalizar nombre

--- Sección 5/aplicacionPipes/src/app/pipes/capitalizado.pipe.ts
+++ Sección 5/aplicacionPipes/src/app/pipes/capitalizado.pipe.ts
@@ -3,10 +3,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'capitalizado'
 })
+
 export class CapitalizadoPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string, seCapitalizanTodasPalabras: boolean = true): string {
+    value = value.toLocaleLowerCase();
+    let nombres = value.split(' ');
+
+    if(seCapitalizanTodasPalabras){
+      nombres = nombres.map(nombre => {
+        return nombre[0].toUpperCase() + nombre.substr(1);
+      });
+    }else{
+      nombres[0] = nombres[0][0].toUpperCase() + nombres[0].substr(1);
+    }
+    return nombres.join(' ');
   }
 
 }
