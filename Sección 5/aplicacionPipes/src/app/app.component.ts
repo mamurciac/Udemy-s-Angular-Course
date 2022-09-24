ce26a76789289d856f2b162fb23bf57cd82a5129
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-09-24 05:35:57 -0500

    Incorporación de un Video con URL indicada en el componente

--- Sección 5/aplicacionPipes/src/app/app.component.ts
+++ Sección 5/aplicacionPipes/src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent {
   salario: number = 1234.5;
   fecha: Date = new Date();
   idioma: string = 'es';
+  videoUrl: string = 'https://www.youtube.com/embed/AVAELaAOkCY';
 
   valorPromesa = new Promise<string>((resolve) => {
     setTimeout(() => {
