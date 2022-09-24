47f6137edbd63184831debeb730811358caacdb8
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-09-24 06:11:45 -0500

    Implementación de Pipe para Mostrar/Ocultar contraseña

--- Sección 5/aplicacionPipes/src/app/pipes/mostrar-contrasena.pipe.ts
+++ Sección 5/aplicacionPipes/src/app/pipes/mostrar-contrasena.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MostrarContrasenaPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string, seMuestraContrasena: boolean): string {
+    return seMuestraContrasena ? value : '*'.repeat(value.length);
   }
 
 }
