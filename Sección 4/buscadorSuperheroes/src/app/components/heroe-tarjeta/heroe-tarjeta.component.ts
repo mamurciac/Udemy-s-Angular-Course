c35de59d2afc5bfa1e085db1fc6854784e920eed
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-09-24 01:00:03 -0500

    Manejo de decorador Input para tener tarjetas como un componente independiente (del body)

--- Sección 4/buscadorSuperheroes/src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ Sección 4/buscadorSuperheroes/src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-heroe-tarjeta',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroe-tarjeta.component.css']
 })
 export class HeroeTarjetaComponent implements OnInit {
+  @Input() heroe: any = {};
+  @Input() index: number = -1;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  verHeroe(){
+    console.log(this.index);
+    this.router.navigate(['/heroe', this.index]);
   }
 
 }
