c593e841ec551a60d7e3a11145c19eba09f5434f
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-09-29 03:59:53 -0500

    Obtención en Consola de Petición de Álbumes

--- Sección 6/Spotiapp/src/app/components/home/home.component.ts
+++ Sección 6/Spotiapp/src/app/components/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent implements OnInit {
   paises: any[] = [];
 
   constructor(private spotify: SpotifyService){
-    
+    this.spotify.getNewReleases();
   }
 
   ngOnInit(): void {
--- Sección 6/Spotiapp/src/app/services/spotify.service.ts
+++ Sección 6/Spotiapp/src/app/services/spotify.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,7 +6,16 @@ import { Injectable } from '@angular/core';
 })
 export class SpotifyService {
 
-  constructor(){
+  constructor(private http: HttpClient){
     console.log('Servicio de Spotify listo para usar!!!');
+  }
+
+  getNewReleases(){
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQBK0UC3WLAGQ_pFEE6H1XZFWp6eoBhgcQCLJGR7_SCpnTOk717G9u2jtKbEmZ3fh01H2hd2fZxmO6AK8CVS_CCb0Wbov3WceSTO80EOpx64AZ6eFvE'
+    });
+    this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {headers}).subscribe(data => {
+      console.log(data);
+    })
   }
 }
