5c6ef5203a1a81b4d2e9128baacb6253062a643c
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-09-29 05:35:15 -0500

    Optimización de Código de Petición usando operador Map

--- Sección 6/Spotiapp/src/app/services/spotify.service.ts
+++ Sección 6/Spotiapp/src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,13 +15,17 @@ export class SpotifyService {
     const headers = new HttpHeaders({
       'Authorization': 'Bearer BQCzgLVfwCQNWo2KBB_D9lTOG-6iqjQHZSZ0RnJW8g66l1mW2zk-mRgJMeCOxBQg1dsvsetrcrOcCfTVYoRqhy-8hm6eoQALuLYl7C8sPwHRAXuc6KI'
     });
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers}).pipe(map((data: any) => {
+      return data['albums'].items;
+    }));
   }
 
   getArtista(termino: string){
     const headers = new HttpHeaders({
       'Authorization': 'Bearer BQCzgLVfwCQNWo2KBB_D9lTOG-6iqjQHZSZ0RnJW8g66l1mW2zk-mRgJMeCOxBQg1dsvsetrcrOcCfTVYoRqhy-8hm6eoQALuLYl7C8sPwHRAXuc6KI'
     });
-    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers});
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers}).pipe(map((data: any) => {
+      return data['artists'].items;
+    }));
   }
 }
