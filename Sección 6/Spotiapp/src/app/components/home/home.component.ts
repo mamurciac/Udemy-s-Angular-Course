528062872790307080dc649aa9e5f5cab536c516
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-09-29 04:39:54 -0500

    Uso de valores de la petición GET para incorporarlos al HTML

--- Sección 6/Spotiapp/src/app/components/home/home.component.ts
+++ Sección 6/Spotiapp/src/app/components/home/home.component.ts
@@ -8,10 +8,13 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 })
 export class HomeComponent implements OnInit {
 
-  paises: any[] = [];
+  nuevasCanciones: any[] = [];
 
   constructor(private spotify: SpotifyService){
-    this.spotify.getNewReleases();
+    this.spotify.getNewReleases().subscribe((data: any) => {
+      console.log(data.albums.items);
+      this.nuevasCanciones = data.albums.items;
+    });
   }
 
   ngOnInit(): void {
--- Sección 6/Spotiapp/src/app/services/spotify.service.ts
+++ Sección 6/Spotiapp/src/app/services/spotify.service.ts
@@ -14,8 +14,6 @@ export class SpotifyService {
     const headers = new HttpHeaders({
       'Authorization': 'Bearer BQBK0UC3WLAGQ_pFEE6H1XZFWp6eoBhgcQCLJGR7_SCpnTOk717G9u2jtKbEmZ3fh01H2hd2fZxmO6AK8CVS_CCb0Wbov3WceSTO80EOpx64AZ6eFvE'
     });
-    this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {headers}).subscribe(data => {
-      console.log(data);
-    })
+    return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
   }
 }
