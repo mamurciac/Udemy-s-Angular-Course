5fe69bb217b55941da1996aec3834ffec4a5b8d0
Michael Andrés Murcia <mamurciac@unal.edu.co>
2022-10-02 00:16:53 -0500

    Incorporación del Loading Component en Búsqueda

--- Sección 6/Spotiapp/src/app/components/search/search.component.ts
+++ Sección 6/Spotiapp/src/app/components/search/search.component.ts
@@ -8,15 +8,20 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 })
 export class SearchComponent implements OnInit {
   artistas: any[] = [];
+  loading: boolean = false;
 
-  constructor(private spotify: SpotifyService) { }
+  constructor(private spotify: SpotifyService) {
+
+  }
 
   buscar(termino: string){
+    this.loading = true;
     console.log(termino);
     this.spotify.getArtista(termino).subscribe((data: any) => {
       console.log(data);
       this.artistas = data;
     })
+    this.loading = false;
   }
 
   ngOnInit(): void {
